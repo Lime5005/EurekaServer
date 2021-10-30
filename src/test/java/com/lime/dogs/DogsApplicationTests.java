0cf193e9f0217f85cac6785aea7f67884839b711
Lime5005 <zlp5005@gmail.com>
2021-10-30 23:57:59 +0200

    Add Integration tests for rest API

--- src/test/java/com/lime/dogs/DogsApplicationTests.java
+++ src/test/java/com/lime/dogs/DogsApplicationTests.java
@@ -1,13 +1,45 @@
 package com.lime.dogs;
 
+import com.lime.dogs.entity.Dog;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class DogsApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	public void getAllDogs() {
+		TestRestTemplate testRestTemplate = this.restTemplate.withBasicAuth("admin", "password");
+		ResponseEntity<Object> response = testRestTemplate.getForEntity("http://localhost:" + port + "/dogs/", Object.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+	}
+
+	@Test
+	public void getADog() {
+		TestRestTemplate testRestTemplate = this.restTemplate.withBasicAuth("admin", "password");
+		ResponseEntity<Dog> response =
+				testRestTemplate.getForEntity("http://localhost:" + port + "/dogs/1", Dog.class);
+
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
 	}
 
 }
