01d3bc876041ea77b944ebe04ccde2b4ca6ef5ce
Lime5005 <zlp5005@gmail.com>
2021-10-30 21:38:00 +0200

    Add Swagger to document rest API

--- src/main/java/com/lime/dogs/config/SecurityConfig.java
+++ src/main/java/com/lime/dogs/config/SecurityConfig.java
@@ -9,9 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@EnableSwagger2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,5 +45,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(true);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Location API",
+                "This API returns a list of airport locations.",
+                "1.0",
+                "http://www.udacity.com/tos",
+                new Contact("Lime", "www.udacity.com", "myaddress@email.com"),
+                "License of API", "http://www.udacity.com/license", Collections.emptyList());
     }
 }
